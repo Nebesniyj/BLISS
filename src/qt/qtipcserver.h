50d83c2a968bc3cbc7b3d5d12c80c88f8acda7ed
Bliss <31364205+Nebesniyj@users.noreply.github.com>
2018-02-17 00:48:10 +0900

    no message

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define BLISS-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "BLISSURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
